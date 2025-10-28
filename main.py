fa854c5c243fd2ca834fdca191a773ccb38b1d54
Ollenotna <118982647+Ollenotna@users.noreply.github.com>
2025-10-28 17:49:30 +0100

    login folder structure

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ import utilities
 def main():
     # Check login state
     if "player_name" not in st.session_state or st.session_state.player_name is None:
-        st.switch_page("login.py")  # 👈 Redirects to login page
+        st.switch_page("pages/login.py")
 
     # If logged in, show main content
-    st.title(f"📊 Welcome {st.session_state.player_name}!")
+    st.title(f"Welcome {st.session_state.player_name}!")
     st.write("Here are your stats...")
 
     if st.button("Logout"):
         st.session_state.player_name = None
-        st.switch_page("login.py")
+        st.switch_page("pages/login.py")
 
 
-    df_scores, df_pairs, giocatori_to_keep = utilities.load_data()
+    #df_scores, df_pairs, giocatori_to_keep = utilities.load_data()
 
     st.title('Gigi')
 
     tabs = st.tabs(["TAB_1", "TAB_2"])
     with tabs[0]:
         # Get plot
-        fig = utilities.performance_plot(df_scores, giocatori_to_keep)
-        st.pyplot(fig=fig)
+        st.text("miao 1")
+        #fig = utilities.performance_plot(df_scores, giocatori_to_keep)
+        #st.pyplot(fig=fig)
     with tabs[1]:
         st.text("miao")
 
