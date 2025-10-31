614aef7c7ca3086cdbef5e6352ca781b53b5cffb
Ollenotna <118982647+Ollenotna@users.noreply.github.com>
2025-10-31 11:59:24 +0100

    screen adapatation

--- statistics_delirio.py
+++ statistics_delirio.py
@@ -201,7 +201,7 @@ with tabs[0]:
                         font-size: 5.5vw !important;
                     }}
                     .bonus-line {{
-                        height: 22vw !important;
+                        height: 24vw !important;
                         border-left-width: 0.8vw !important;
                     }}
                     .malus-line {{
