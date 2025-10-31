06ff5ebd23c5fc65e2216acf78a1bdf905c507f4
Ollenotna <118982647+Ollenotna@users.noreply.github.com>
2025-10-31 11:58:20 +0100

    screen adapatation

--- statistics_delirio.py
+++ statistics_delirio.py
@@ -201,9 +201,11 @@ with tabs[0]:
                         font-size: 5.5vw !important;
                     }}
                     .bonus-line {{
+                        height: 18vw !important;
                         border-left-width: 0.8vw !important;
                     }}
                     .malus-line {{
+                        height: 6vw !important;
                         border-left-width: 0.8vw !important;
                     }}
                 }}
@@ -221,7 +223,7 @@ with tabs[0]:
             ">
                 <div class="label-text" style="font-size: 1.8vh; color: gray; margin-bottom: 1.5vh;">Bonus/Malus</div>
                 <div style="display: flex; align-items: center; justify-content: center; margin-bottom: 1vh; width: 100%;">
-                    <div class="bonus-line" style="border-left: 0.4vw solid #4CAF50; margin-right: 1vw;"></div>
+                    <div class="bonus-line" style="border-left: 0.4vw solid #4CAF50; margin-right: 1vw; height: 12vh;"></div>
                     <div class="value-text" style="display: flex; flex-direction: column; gap: 0.5vh; font-size: 2.5vh;">
                         <div style="display: flex; justify-content: space-between; min-width: 80px;">
                             <span>🥩</span>
@@ -238,7 +240,7 @@ with tabs[0]:
                     </div>
                 </div>
                 <div style="display: flex; align-items: center; justify-content: center; width: 100%;">
-                    <div class="malus-line" style="border-left: 0.4vw solid #f44336; margin-right: 1vw;"></div>
+                    <div class="malus-line" style="border-left: 0.4vw solid #f44336; margin-right: 1vw; height: 4vh;"></div>
                     <div class="value-text" style="display: flex; justify-content: space-between; min-width: 80px; font-size: 2.5vh;">
                         <span>💔</span>
                         <span style="text-align: right; margin-left: 10px;">{sel_pt_malus}</span>
